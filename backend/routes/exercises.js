51e19fb28fc404ad07bc18fc6a13e6903b667cf5
gregroyclark <gregroyclark@gmail.com>
2020-12-27 14:18:06 -0600

    fixes bug @ line 50: adds .json to res.status(400)

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -47,7 +47,7 @@ router.route('/update/:id').post((req, res) => {
 
         exercise.save()
           .then(() => res.json('Exercise updated!'))
-          .catch(err => res.status(400)('Error: ' + err));
+          .catch(err => res.status(400).json('Error: ' + err));
       })
       .catch(err => res.status(400).json('Error: ' + err)); 
 });
