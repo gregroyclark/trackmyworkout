038e05d5316ca76b0dbaf6f1aee13c1ee322105e
gregroyclark <gregroyclark@gmail.com>
2021-01-01 22:11:56 -0600

    adds constructor

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 export default class CreateExercise extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
     render() {
         return (
             <div>
